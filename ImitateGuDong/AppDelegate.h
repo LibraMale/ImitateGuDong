8666d7d5fbaffa98a957dadb327b53e0748220e7
刘鸿鹏 <libramale@163.com>
2016-10-21 20:16:13 +0800

    创建项目

---
+++ ImitateGuDong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImitateGuDong
+//
+//  Created by LibraLiu on 16/10/21.
+//  Copyright © 2016年 LL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
